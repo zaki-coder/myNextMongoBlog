8304504cad5ecd4a7ed7ca15d610b9b240b58a58
zaki <zakiazizi1841992@gmail.com>
2023-09-16 19:55:41 +0200

    add auth

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -3,18 +3,27 @@
 import Link from "next/link";
 import styles from "./authLinks.module.css";
 import { useState } from "react";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const AuthLinks = () => {
     const [open, setOpen] = useState(false);
+    const { data, status } = useSession();
 
-    const status = "authenticated";
+    const router = useRouter();
+
+    if(status === "unauthenticated") {
+        router.push("/login");
+    }
+
+    // const status = "authenticated";
     return <>
         {status === "notauthenticated" ? (
             <Link href="/login" className={styles.link}>Login</Link>
         ): (
             <>
                 <Link href="/write" className={styles.link}>Write</Link>
-                <span className={styles.link}>
+                <span className={styles.link} onClick={signOut}>
                     Logout
                 </span>
             </>
---
+++ src/utils/auth.js
@@ -0,0 +1,16 @@
+
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_SECRET,
+        }),
+        GithubProvider({
+          clientId: process.env.GITHUB_ID,
+          clientSecret: process.env.GITHUB_SECRET,
+        }),
+      ],
+  };
