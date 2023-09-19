9ce269274cff23515a2e2aee64dc07110a2cf443
zaki <zakiazizi1841992@gmail.com>
2023-09-19 16:37:21 +0200

    fetch posts

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,20 +5,23 @@ import Image from "next/image";
 const Card = ({key, item}) => {
   return (
     <div className={styles.container} key={key}>
+      {item.img && (
         <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
-          </div>
+          <Image src={item.img} alt="" fill className={styles.image} />
+        </div>
+
+      )}
         <div className={styles.textContainer}>
             <div className={styles.detail}>
-                <span className={styles.date}>11.03.2023 - </span>
-                <span className={styles.category}>CULTURE</span>
+                <span className={styles.date}>{item.createdAt.substring(0, 10)} - </span>
+                <span className={styles.category}>{item.catSlug}</span>
             </div>
-            <Link href="/">
+            <Link href={`/posts/${item.slug}`}>
                 <h1>{item.title}</h1>
             </Link>
             
-            <p className={styles.desc}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque ullam delectus, cum et quia eos unde, voluptate esse, tempore dolores optio rem hic cupiditate qui!</p>
-            <Link href="/" className={styles.link}>Read More</Link>
+            <p className={styles.desc}>{item.createdAt.substring(0, 10)}</p>
+            <Link href={`/posts/${item.slug}`} className={styles.link}>Read More</Link>
         </div>
     </div>
   )
